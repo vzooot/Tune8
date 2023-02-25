007554454725dcfd0ead86da02d84c6aae68ce7f
krak <krak@krak.com>
2023-02-25 09:43:33 +0100

    Add icons and color theme

---
+++ Tune8/Extensions/Color.swift
@@ -0,0 +1,21 @@
+//
+//  Color.swift
+//  Tune8
+//
+//  Created by Administrator on 2/25/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackGroundColor")
+    let blue = Color("BlueColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
