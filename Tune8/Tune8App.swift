7c5fd2eaa30760fc464f4efef8bbb446045fc6ee
vzooot <vzotikas@hotmail.com>
2023-02-25 09:23:39 +0100

    Initial Commit

---
+++ Tune8/Tune8App.swift
@@ -0,0 +1,17 @@
+//
+//  Tune8App.swift
+//  Tune8
+//
+//  Created by Administrator on 2/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Tune8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
